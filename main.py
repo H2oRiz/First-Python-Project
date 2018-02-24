787c3aa4ab3932d76a22bc88ab36829c2cd77c97
H2oRiz <h2orizwan@gmail.com>
2018-02-24 03:04:47 +0000

    Going through youtube tutorial https://www.youtube.com/watch?v=kcdyxT3se1Y

---
+++ main.py
@@ -0,0 +1,11 @@
+def whattosay():
+    print("Hello World")
+
+def add(num1 , num2):
+    return num1 + num2
+
+
+saythis = input("What should I repeat 10 times?\n")
+
+for i in range(0,9):
+    print(saythis)
